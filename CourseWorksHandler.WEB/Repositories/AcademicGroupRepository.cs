e43b7f9921cc3fe4cbc0204f281fce57093a417c
EdShel <eduardchelemetjev@gmail.com>
2020-10-21 17:39:43 +0300

    Created groups repository

---
+++ CourseWorksHandler.WEB/Repositories/AcademicGroupRepository.cs
@@ -0,0 +1,44 @@
+﻿using CourseWorksHandler.WEB.Models;
+using System;
+using System.Data.SqlClient;
+
+namespace CourseWorksHandler.WEB.Repositories
+{
+    public sealed class AcademicGroupRepository : BasicAsyncRepository<AcademicGroup>
+    {
+        public AcademicGroupRepository(SqlConnection sqlConnection) : base(sqlConnection)
+        {
+        }
+
+        protected override Func<SqlDataReader, AcademicGroup> SelectMapper
+        {
+            get => r => new AcademicGroup
+            {
+                Id = r.GetInt32(0),
+                GroupName = r.GetString(1),
+                TeacherId = r.GetInt32(2)
+            };
+        }
+
+        protected override Func<AcademicGroup, object[]> InsertValues
+        {
+            get => group => new object[]
+            {
+                group.GroupName, group.TeacherId
+            };
+        }
+
+        protected override Func<AcademicGroup, TablePropertyValuePair[]> UpdatePropertiesAndValuesExtractor
+        {
+            get => grop => new TablePropertyValuePair[]{
+                new TablePropertyValuePair("GroupName", grop.GroupName),
+                new TablePropertyValuePair("TeacherId", grop.TeacherId)
+            };
+        }
+
+        protected override TablePropertyExtractor UpdatePredicatePropertyEqualsValue
+        {
+            get => new TablePropertyExtractor("Id", t => t.Id);
+        }
+    }
+}
