f929e482baed9319ffc8166159cd62ff5a89d8b6
EdShel <eduardchelemetjev@gmail.com>
2020-10-21 17:36:32 +0300

    Created students repository

---
+++ CourseWorksHandler.WEB/Repositories/StudentRepository.cs
@@ -0,0 +1,46 @@
+﻿using CourseWorksHandler.WEB.Models;
+using System;
+using System.Data.SqlClient;
+
+namespace CourseWorksHandler.WEB.Repositories
+{
+    public sealed class StudentRepository : BasicAsyncRepository<Student>
+    {
+        public StudentRepository(SqlConnection sqlConnection) : base(sqlConnection)
+        {
+        }
+
+        protected override Func<SqlDataReader, Student> SelectMapper
+        {
+            get => r => new Student
+            {
+                Id = r.GetInt32(0),
+                FullName = r.GetString(1),
+                GroupId = r.GetInt32(2),
+                Mark = r.GetInt32(3)
+            };
+        }
+
+        protected override Func<Student, object[]> InsertValues
+        {
+            get => student => new object[]
+            {
+                student.FullName, student.GroupId, student.Mark
+            };
+        }
+
+        protected override Func<Student, TablePropertyValuePair[]> UpdatePropertiesAndValuesExtractor
+        {
+            get => student => new TablePropertyValuePair[]
+            {
+                new TablePropertyValuePair("FullName", student.FullName),
+                new TablePropertyValuePair("Mark", student.Mark)
+            };
+        }
+
+        protected override TablePropertyExtractor UpdatePredicatePropertyEqualsValue
+        {
+            get => new TablePropertyExtractor("Id", st => st.Id);
+        }
+    }
+}
